229157a8fd98f568b19d3877ea555e58f6793307
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2022-11-17 17:30:43 +0100

    feat: create and style header component

---
+++ laughportal/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import laugh from '../images/laugh.svg'
+
+function Header() {
+    return <header>
+        <div>
+            <img src={laugh} alt='laughing emoji'></img>
+            <p>Laugh portal</p>
+        </div>
+        <button type='submit'>send a laugh</button>
+    </header>
+}
+export default Header
